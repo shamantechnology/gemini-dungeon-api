0194634b13e2a101f646fa629e3beacd37e8e2f8
risingsunomi <vincentcastro@gmail.com>
2023-12-20 15:31:10 -0500

    completeing the api end, need work on convo memory, need to load in story.txt to vectorestorage for reference

--- src/app.py
+++ src/app.py
@@ -4,41 +4,116 @@ Gemini Dungeon API
 Game logic, chat and image generation api for gemini-dungeon frontend
 """
 from datetime import datetime
-
 from flask import Flask, request, jsonify, make_response
-
 from flask_cors import CORS
-
-from dndlibrary import DNDLibrary
-
+from dotenv import load_dotenv
 import logging
+import json
+
+from geminidm import GeminiDM
+from stabilityapi import StabilityAPI
 
 logging.basicConfig(format="[%(asctime)s] %(name)s - %(levelname)s - %(message)s")
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/dmstart", methods=["POST"])
+def dmstart():
+    """
+    Starts the process and creates the first message and image
+    """
+    
+    user_msg = "start"
+    reply_dict = {
+        "ai": "",
+        "vision": "",
+        "error": ""
+    }
+
+    try:
+        ai_resp = gdm.chat(user_msg=user_msg)
+    except Exception as err:
+        logger.error(err)
+        reply_dict["error"] = "system error"
+        json_reply = jsonify(reply_dict)
+        return make_response(json_reply, 500)
+    
+    print(ai_resp)
+
+    try:
+        ai_json = json.loads(ai_resp)
+    except json.JSONDecodeError as err:
+        logger.error(err)
+        reply_dict["error"] = "system error - json failed from AI"
+        json_reply = jsonify(reply_dict)
+        return make_response(json_reply, 500)
+
+    try:
+        sapi_reply = sapi.generate_image(ai_json["view"])
+    except Exception as err:
+        logger.error(err)
+        reply_dict["error"] = "system error - stability failed"
+        json_reply = jsonify(reply_dict)
+        return make_response(json_reply, 500)
+
+    reply_dict["ai"] = ai_json["content"]
+    reply_dict["vision"] = sapi_reply["artifacts"][0]["base64"]
+    json_reply = jsonify(reply_dict)
+    return make_response(json_reply, 201)
+
 
 @app.route("/run", methods=["POST"])
 def run():
+    user_msg = request.json["usermsg"]
+    reply_dict = {
+        "ai": "",
+        "vision": "",
+        "error": ""
+    }
+
     try:
-        # logging.info("Setting up DND library")
-        # dndlib = DNDLibrary()
-        # dndlib.run()
-
-        logger.info("Starting DM Gemini")
-
+        ai_resp = gdm.chat(user_msg=user_msg)
     except Exception as err:
         logger.error(err)
-        raise
+        reply_dict["error"] = "system error"
+        json_reply = jsonify(reply_dict)
+        return make_response(json_reply, 500)
+    
+    print(ai_resp)
 
-    json_reply = jsonify({"ai": "test", "vision": "asvbcds"})
+    try:
+        ai_json = json.loads(ai_resp)
+    except json.JSONDecodeError as err:
+        logger.error(err)
+        reply_dict["error"] = "system error - json failed from AI"
+        json_reply = jsonify(reply_dict)
+        return make_response(json_reply, 500)
+
+    try:
+        sapi_reply = sapi.generate_image(ai_json["view"])
+    except Exception as err:
+        logger.error(err)
+        reply_dict["error"] = "system error - stability failed"
+        json_reply = jsonify(reply_dict)
+        return make_response(json_reply, 500)
+
+    reply_dict["ai"] = ai_json["content"]
+    reply_dict["vision"] = sapi_reply["artifacts"][0]["base64"]
+    json_reply = jsonify(reply_dict)
     return make_response(json_reply, 201)
 
 
 if __name__ == "__main__":
+    load_dotenv()
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
+
+    logger.info("Starting GeminiDM module")
+    gdm = GeminiDM()
+
+    logger.info("starting stability.ai API")
+    sapi = StabilityAPI()
 
     # start med coder with api start
     dt_run = datetime.now().strftime("%m%d%Y %H:%M:%s")
--- src/geminidm.py
+++ src/geminidm.py
@@ -15,9 +15,11 @@ import faiss
 from langchain.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.embeddings import VertexAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.docstore import InMemoryDocstore
 from langchain.vectorstores import FAISS
 from langchain.memory import VectorStoreRetrieverMemory
+from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 
 from trulens_eval import Feedback, LiteLLM, TruChain
@@ -28,6 +30,7 @@ from player import Player
 class GeminiDM:
     def __init__(self, player: any = None):
         self.instruction_prompt_path = Path("prompts/dmstart.txt")
+        self.story_path = Path("data/story.txt")
         self.player = player if player else Player()
         self.llm = ChatGoogleGenerativeAI(temperature=0.1, model="gemini-pro")
         self.conversation = None
@@ -37,37 +40,60 @@ class GeminiDM:
         self.class_logger = logging.getLogger(__name__)
         self.class_logger.setLevel(logging.DEBUG)
 
+        # get story
+        story_txt = ""
+        with open(self.story_path) as txtfile:
+            for tline in txtfile.readlines():
+                story_txt += tline
+
         # setup instruction prompt
         prompt_txt = ""
         with open(self.instruction_prompt_path) as txtfile:
-            prompt_txt += txtfile.readline()
+            for tline in txtfile.readlines():
+                prompt_txt += tline
 
         self.instruction_prompt_template = PromptTemplate(
-            input_variables=["player", "history"], template=prompt_txt
+            input_variables=["history", "input"], template=prompt_txt
         )
 
-        self.instruction_prompt = self.instruction_prompt_template.format(
-            player=self.player.player_sheet(), history=""
-        )
+        # self.memory = ConversationBufferMemory()
+
+        # print(self.instruction_prompt_template)
+
+        # self.instruction_prompt = self.instruction_prompt_template.format(
+        #     player=self.player.player_sheet(), history=""
+        # )
 
         # setup chat vectorstore
         # this is using faiss-cpu
+        embedding_size = 768
         self.vectorstore = FAISS(
             VertexAIEmbeddings().embed_query,
-            faiss.IndexFlatL2(),
+            # HuggingFaceEmbeddings().embed_query,
+            faiss.IndexFlatL2(embedding_size),
             InMemoryDocstore({}),
             {},
         )
 
         # setup memory
+        retriever = self.vectorstore.as_retriever(search_kwargs=dict(k=1))
         self.memory = VectorStoreRetrieverMemory(
-            retriever=self.vectorstore.as_retriever(search_kwargs=dict(k=1))
+            retriever=retriever
+        )
+
+        self.memory.save_context(
+            {"System": f"Player Information\n{self.player.player_sheet()}"}, {"AI": ""}
+        )
+
+
+        self.memory.save_context(
+            {"System": f"The story\n{story_txt}"}, {"AI": ""}
         )
 
         # creating llm chain
         self.conversation = ConversationChain(
             llm=self.llm,
-            prompt=self.instruction_prompt,
+            prompt=self.instruction_prompt_template,
             memory=self.memory,
             verbose=True,
         )
@@ -79,31 +105,33 @@ class GeminiDM:
         litellm_provider = LiteLLM(model_engine="chat-bison")
         feedbacks.append(Feedback(litellm_provider.conciseness).on_output())
 
-        self.chain_recorder = TruChain(
-            self.conversation, app_id="med-coder-llm", feedbacks=feedbacks
-        )
+        # self.chain_recorder = TruChain(
+        #     self.conversation,
+        #     app_id="med-coder-llm",
+        #     feedbacks=feedbacks
+        # )
 
     def chat(self, user_msg: str) -> str:
         """
         String input to gemini chat from user
         Record chat interaction and test for conciseness with TruLens
         """
-        tru_record = None
-        with self.chain_recorder as recorder:
-            resp = self.conversation.invoke(user_msg)
-            tru_record = recorder.get()
+        # tru_record = None
+        # with self.chain_recorder as recorder:
+        resp = self.conversation.invoke(user_msg)
+        #     tru_record = recorder.get()
 
-        conciseness_val = 1.0
-        if tru_record:
-            for feedback_future in as_completed(tru_record.feedback_results):
-                feedback, feedback_result = feedback_future.result()
+        # conciseness_val = 1.0
+        # if tru_record:
+        #     for feedback_future in as_completed(tru_record.feedback_results):
+        #         feedback, feedback_result = feedback_future.result()
 
-                if feedback.name == "conciseness":
-                    conciseness_val = (
-                        float(feedback_result.result) if feedback_result.result else 1.0
-                    )
+        #         if feedback.name == "conciseness":
+        #             conciseness_val = (
+        #                 float(feedback_result.result) if feedback_result.result else 1.0
+        #             )
 
-        if conciseness_val < 0.5:
-            return "I don't not understand...."
-        else:
-            return resp.content
+        # if conciseness_val < 0.5:
+        #     return "I don't not understand...."
+        # else:
+        return resp["response"]
